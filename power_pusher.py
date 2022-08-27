1bcb625b9373d68aa5a03b18e30748c798035960
Brock A. Martin <bamartin125@gmail.com>
2022-08-27 18:56:22 +0100

    Add log level option

--- power_pusher.py
+++ power_pusher.py
@@ -237,8 +237,13 @@ def power_off(*, index, hold_seconds=DEFAULT_POWER_OFF_SECONDS,):
 
 
 @click.group()
-def cli():
-    logging.basicConfig(level='DEBUG')
+@click.option(
+    '--log-level',
+    type=lambda l: l.upper()
+    default='ERROR',
+)
+def cli(log_level):
+    logging.basicConfig(level=log_level)
 
 
 @cli.command('power-hold')
